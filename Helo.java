c19250c2ae6dd38224ea6dd9f5b0d65bdb5d45ec
NaliniSekar <NaliniSekar@users.noreply.github.com>
2016-07-18 01:53:36 -0700

    Update and rename README.md to Helo.java

---
+++ Helo.java
@@ -0,0 +1,11 @@
+# world
+Public static void main(String args[])
+{
+System.out.println("enter num");
+Scanner in = new Scanner();
+int b=in.nextInt();
+for(int i=1;i<=5;i++)
+{
+System.out.println("hello world!!!");
+}
+}
