9702cc38ba3a41eba84d6694f842a95eeb10c8db
NaliniSekar <NaliniSekar@users.noreply.github.com>
2016-07-18 01:55:09 -0700

    Update Helo.java

--- Helo.java
+++ Helo.java
@@ -1,9 +1,7 @@
 # world
 Public static void main(String args[])
 {
-System.out.println("enter num");
-Scanner in = new Scanner();
-int b=in.nextInt();
+
 for(int i=1;i<=5;i++)
 {
 System.out.println("hello world!!!");
